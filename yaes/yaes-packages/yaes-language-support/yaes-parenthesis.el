cf8d57ddac9a337653470431e51f8c657b2061d7
Ailrun <jjc9310@gmail.com>
2016-11-03 16:04:13 +0900

    Move parenhighlight

--- yaes/yaes-packages/yaes-language-support/yaes-parenthesis.el
+++ yaes/yaes-packages/yaes-language-support/yaes-parenthesis.el
@@ -10,16 +10,21 @@
   ;; required emacs version : ???
   :require (dash cl-lib)
   :config (progn (smartparens-global-mode t)
-				 (sp-with-modes '(emacs-lisp-mode lisp-mode)
-				   (sp-local-pair "'" nil :actions nil)
-				   (sp-local-pair "`" nil :actions nil))
-				 (sp-with-modes '(LaTeX-mode latex-mode)
-				   (sp-local-pair "`" "'")
-				   (sp-local-pair "'" nil :actions nil))
-				 (sp-with-modes '(coq-mode)
-				   (sp-local-pair "'" nil :actions nil))
-				 (sp-with-modes '(haskell-mode)
-				   (sp-local-pair "'" nil :actions nil))))
+                 (sp-with-modes '(emacs-lisp-mode lisp-mode)
+                   (sp-local-pair "'" nil :actions nil)
+                   (sp-local-pair "`" nil :actions nil))
+                 (sp-with-modes '(LaTeX-mode latex-mode)
+                   (sp-local-pair "`" "'")
+                   (sp-local-pair "$" "$")
+                   (sp-local-pair "'" nil :actions nil))
+                 (sp-with-modes '(coq-mode)
+                   (sp-local-pair "'" nil :actions nil))
+                 (sp-with-modes '(haskell-mode)
+                   (sp-local-pair "'" nil :actions nil))))
+
+(req-package highlight-parentheses
+  :config
+  (global-highlight-parentheses-mode t))
 
 (provide 'yaes-parenthesis)
 ;;; yaes-parenthesis.el ends here
