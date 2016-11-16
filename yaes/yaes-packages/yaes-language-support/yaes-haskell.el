edee10ba0ed082735a38f2b828455b5cf7b6f82f
Ailrun <jjc9310@gmail.com>
2016-11-16 18:09:12 +0900

    Update haskell enviroment to intero

--- yaes/yaes-packages/yaes-language-support/yaes-haskell.el
+++ yaes/yaes-packages/yaes-language-support/yaes-haskell.el
@@ -12,12 +12,21 @@
   ;; How about .lhs??
   :mode ("\\.l?hs\\'" . haskell-mode))
 
-;; Plugin for flycheck with Haskell
-(req-package flycheck-haskell
+(req-package intero
   ;; required emacs version : >= 24.3
-  :require (flycheck haskell-mode dash seq let-alist)
-  :commands (flycheck-haskell-setup)
-  :init (add-hook-exec 'flycheck-mode #'flycheck-haskell-setup))
+  :require (flycheck company haskell-mode)
+  :commands (intero-mode)
+  :init (add-hook-exec 'haskell-mode #'intero-mode)
+  :config (progn
+            (setq-default intero-repl-no-load nil)
+            (setq-default intero-repl-no-build nil)))
+
+;; ;; Plugin for flycheck with Haskell
+;; (req-package flycheck-haskell
+;;   ;; required emacs version : >= 24.3
+;;   :require (flycheck haskell-mode dash seq let-alist)
+;;   :commands (flycheck-haskell-setup)
+;;   :init (add-hook-exec 'flycheck-mode #'flycheck-haskell-setup))
 
 ;; ;; Indentation support for haskell mode
 ;; (req-package hi2
@@ -26,24 +35,24 @@
 ;;   :commands (turn-on-hi2)
 ;;   :init (add-hook-exec 'haskell-mode #'turn-on-hi2))
 
-;; Emacs binding for ghc-mod program
-;; Must install ghc-mod using "cabal install ghc-mod"
-(req-package ghc
-  ;; required emacs version : ???
-  :require (haskell-mode)
-  :commands (ghc-init ghc-debug)
-  :init (add-hook-exec 'haskell-mode #'ghc-init))
+;; ;; Emacs binding for ghc-mod program
+;; ;; Must install ghc-mod using "cabal install ghc-mod"
+;; (req-package ghc
+;;   ;; required emacs version : ???
+;;   :require (haskell-mode)
+;;   :commands (ghc-init ghc-debug)
+;;   :init (add-hook-exec 'haskell-mode #'ghc-init))
 
 ;; ;; Plugin for flycheck using ghc-mod
 ;; (req-package flycheck-ghcmod
 ;;  ;; required emacs version : ???
 ;;  :require (flycheck dash haskell-mode))
 
-;; Backend plugin for company using ghc-mod
-(req-package company-ghc
-  ;; required emacs version : >= 24
-  :require (cl-lib company ghc)
-  :config (add-to-list 'company-backends #'company-ghc))
+;; ;; Backend plugin for company using ghc-mod
+;; (req-package company-ghc
+;;   ;; required emacs version : >= 24
+;;   :require (cl-lib company ghc)
+;;   :config (add-to-list 'company-backends #'company-ghc))
 
 ;; Plugin for yasnippet with haskell mode
 (req-package haskell-snippets
