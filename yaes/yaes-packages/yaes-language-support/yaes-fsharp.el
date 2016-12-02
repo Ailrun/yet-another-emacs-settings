93ed270e4abc8dd2213556a7c3cd3afd5a5e4e02
Ailrun <jjc9310@gmail.com>
2016-12-02 19:50:13 +0900

    Fix fsharp extension

--- yaes/yaes-packages/yaes-language-support/yaes-fsharp.el
+++ yaes/yaes-packages/yaes-language-support/yaes-fsharp.el
@@ -9,7 +9,17 @@
 (req-package fsharp-mode
   ;; required emacs version : ???
   :require (company company-quickhelp popup pos-tip s dash flycheck)
-  :mode ("\\.fs[iylx]\\'" . fsharp-mode))
+  :mode ("\\.fs[iylx]?\\'" . fsharp-mode)
+  :config (if (eq system-type 'windows-nt)
+              (progn
+                (setq inferior-fsharp-program "\"C:\\Program Files (x86)\\Microsoft SDKs\\F#\\4.0\\Framework\\v4.0\\Fsi.exe\"")
+                (setq fsharp-compiler "\"C:\\Program Files (x86)\\Microsoft SDKs\\F#\\4.0\\Framework\\v4.0\\Fsc.exe\""))
+            (progn
+              (setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
+              (setq fsharp-compiler "/usr/bin/fsharpc"))))
+
+;; Add omnisharp if possible.
+;; Reference: https://github.com/OmniSharp/omnisharp-emacs
 
 (provide 'yaes-fsharp)
 ;;; yaes-fsharp.el ends here
