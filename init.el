05a59a167178e3e843e33bbb5166e3527c2bb16f
Ailrun <jjc9310@gmail.com>
2016-12-18 02:49:26 +0900

    Fix init bug

--- init.el
+++ init.el
@@ -10,33 +10,17 @@
 
 (load "~/.emacs.d/external/ProofGeneral/generic/proof-site")
 
-(load-file "~/.emacs.d/yaes/yaes-init.el")
-(require 'f)
-
-(defconst init-file-path (f-this-file))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(defconst init-file-path (expand-file-name "init.el" user-emacs-directory))
+   (when (file-exists-p custom-file)
+       (load custom-file))
 
 (global-set-key (kbd "C-<f1>")
         (lambda ()
           (interactive)
           (load-file init-file-path)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
-     "8577da1641ed4bdf255341ca92e3d0e49c9f4d574458f09ce78159690442cade"
-     default)))
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load-file "~/.emacs.d/yaes/yaes-init.el")
 
 ;; ;; There is no needs for providing init
 ;; (provide 'init)
