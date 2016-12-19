dd2f6f9f09265e53508e240b5f007d02536a5795
Ailrun <jjc9310@gmail.com>
2016-12-20 05:28:23 +0900

    Add yaml

---
+++ yaes/yaes-installable/yaes-languages-support/yaes-yaml.el
@@ -0,0 +1,14 @@
+;;; yaes-yaml --- yaml related package setting of yaes
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'req-package)
+
+(req-package yaml-mode
+  :if (version<= "24.1" emacs-version)
+  :mode ("\\.yml\\'" . yaml-mode))
+
+(provide 'yaes-yaml)
+;;; yaes-yaml.el ends here
