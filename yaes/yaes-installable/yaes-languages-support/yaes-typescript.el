f4b5a3f9f1c292e3a83bced516b98c72668cc09a
Ailrun <jjc9310@gmail.com>
2016-12-20 05:27:32 +0900

    Add typescript

---
+++ yaes/yaes-installable/yaes-languages-support/yaes-typescript.el
@@ -0,0 +1,29 @@
+;;; yaes-typescript --- typescript related package setting of yaes
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'req-package)
+
+(req-package typescript-mode
+  :mode ("\\.ts\\'" . typescript-mode))
+
+(req-package tide
+  :if (executable-find "node")
+  :require (dash flycheck typescript-mode cl-lib)
+  :commands (tide-setup)
+  :init
+  (defun setup-tide-mode ()
+    "tide mode setup."
+    (interactive)
+    (tide-setup)
+    (flycheck-mode t)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode t)
+    (tide-hl-identifier-mode t)
+    (company-mode t))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
+(provide 'yaes-typescript)
+;;; yaes-typescript.el ends here
