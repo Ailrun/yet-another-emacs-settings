41916d5ae057434cc31802be0fa6b8928b719079
Ailrun <jjc9310@gmail.com>
2017-01-09 11:43:14 +0900

    Fix for non-gui environment

--- yaes/yaes-installable/yaes-editor-design/yaes-font.el
+++ yaes/yaes-installable/yaes-editor-design/yaes-font.el
@@ -9,12 +9,13 @@
 (defun yaes-font-setting (font face &optional attr)
   "If FONT is available, change FACE to use FONT with ATTR.
 If FONT is not available, change FACE with ATTR."
-  (let ((set-face-args `(,face nil)))
-    (when (x-list-fonts font)
-        (nconc set-face-args `(:font ,font)))
-    (when attr
-        (nconc set-face-args attr))
-    (apply 'set-face-attribute set-face-args)))
+  (if (not (null window-system))
+      (let ((set-face-args `(,face nil)))
+        (when (x-list-fonts font)
+          (nconc set-face-args `(:font ,font)))
+        (when attr
+          (nconc set-face-args attr))
+        (apply 'set-face-attribute set-face-args))))
 
 ;;;; Font settings
 ;;;;
