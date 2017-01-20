0100dc3d3e6ac7450492747c859688a103081dae
Ailrun <jjc9310@gmail.com>
2017-01-21 02:24:24 +0900

    Add ibuffer-vc

--- yaes/yaes-installable/yaes-editing-functionality/yaes-ibuffer.el
+++ yaes/yaes-installable/yaes-editing-functionality/yaes-ibuffer.el
@@ -12,5 +12,27 @@
   :bind
   (("C-x C-b" . ibuffer)))
 
+(req-package ibuffer-vc
+  :require (ibuffer cl-lib)
+  :commands (ibuffer-vc-set-filter-groups-by-vc-root)
+  :init
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic))))
+  :config
+  (add-to-list 'ibuffer-formats
+               '(mark modified read-only vc-status-mini " "
+                      (name 18 18 :left :elide)
+                      " "
+                      (size 9 -1 :right)
+                      " "
+                      (mode 16 16 :left :elide)
+                      " "
+                      (vc-status 8 8 :left)
+                      " "
+                      filename-and-process)))
+
 (provide 'yaes-ibuffer)
 ;;; yaes-ibuffer.el ends here
