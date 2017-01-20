81c9422936bf10a28830942286473a333231f7f4
Ailrun <jjc9310@gmail.com>
2017-01-21 02:24:03 +0900

    Add coq-inferior support

--- yaes/yaes-installable/yaes-languages-support/yaes-coq.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-coq.el
@@ -6,6 +6,20 @@
 
 (require 'req-package)
 
+(defvar-local yaes-coq-coqtop-path (executable-find "coqtop"))
+
+(if yaes-coq-coqtop-path
+    (progn
+      (eval-and-compile
+        (defun yaes-coq-emacs-lisp-path ()
+          (f-join (f-dirname (f-dirname yaes-coq-coqtop-path)) "emacs")))
+
+      (req-package coq-inferior
+        :force t
+        :load-path (lambda () (list (yaes-coq-emacs-lisp-path)))
+        :ensure nil
+        :commands (run-coq))))
+
 (req-package coq-commenter
   :require (dash s cl-lib)
   :commands (coq-commenter-mode)
