471cfb006882aa4770ba276add035a755dd5bc6c
ailrun <jjc9310@gmail.com>
2017-01-20 15:13:13 +0900

    Fix keymapping using ad-hoc rules

--- yaes/yaes-installable/yaes-editing-functionality/yaes-multiple-cursor.el
+++ yaes/yaes-installable/yaes-editing-functionality/yaes-multiple-cursor.el
@@ -8,16 +8,21 @@
 
 (req-package multiple-cursors
   :require (cl-lib)
+  :init
+  (eval-after-load "multiple-cursors-core"
+    '(progn
+       (define-key mc/keymap (kbd "<return>") nil)
+       (define-key mc/keymap (kbd "C-j") 'multiple-cursors-mode)))
   :bind
-  (("C-S-c C-S-c" . mc/edit-lines)
-   ("C->" . mc/mark-next-like-this)
-   ("C-<" . mc/mark-previous-like-this)
-   ("C-*" . mc/mark-all-like-this)))
+  ("C-S-c C-S-c" . mc/edit-lines)
+  ("C->" . mc/mark-next-like-this)
+  ("C-<" . mc/mark-previous-like-this)
+  ("C-*" . mc/mark-all-like-this))
 
 (req-package phi-search
   :bind
-  (("C-s" . phi-search)
-   ("C-r" . phi-search-backword)))
+  ("C-s" . phi-search)
+  ("C-r" . phi-search-backword))
 
 (provide 'yaes-multiple-cursor)
 ;;; yaes-multiple-cursor.el ends here
