85278fe2ac3faee78a4e22af9705825cddd25d0f
Ailrun <jjc9310@gmail.com>
2018-01-16 16:14:30 -0500

    Add shell here

---
+++ yaes/yaes-installable/yaes-developing-tool/yaes-shell.el
@@ -0,0 +1,12 @@
+;;; yaes-shell --- shell related package setting of yaes
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'req-package)
+
+(req-package shell-here)
+
+(provide 'yaes-shell)
+;;; yaes-shell.el ends here
