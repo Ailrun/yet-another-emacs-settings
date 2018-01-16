c4a0d8a1a54095b44ba15020bab0c61ecc9bc50f
Ailrun <jjc9310@gmail.com>
2018-01-16 16:17:19 -0500

    Remove ido-ubiquitous (should add ido-completing-read+)

--- yaes/yaes-installable/yaes-editing-functionality/yaes-ido.el
+++ yaes/yaes-installable/yaes-editing-functionality/yaes-ido.el
@@ -13,18 +13,18 @@
 
 (req-package ido-completing-read+
   :if (version<= "24.1" emacs-version)
-  :require (cl-lib))
+  :require (cl-lib)
+  :config
+  (setcar ido-cr+-nil-def-alternate-behavior-list
+          "\\`describe-\\(function\\|variable\\|package\\)\\'")
+  (ido-ubiquitous-mode t))
 
 (req-package ido-yes-or-no
   :require (ido-completing-read+)
   :config
+  (add-to-list 'ido-cr+-nil-def-alternate-behavior-list
+               'ido-yes-or-no-mode-p)
   (ido-yes-or-no-mode t))
-
-(req-package ido-ubiquitous
-  :if (version<= "24.1" emacs-version)
-  :require (ido-completing-read+ cl-lib)
-  :config
-  (ido-ubiquitous-mode t))
 
 (req-package smex
   :if (version<= "24" emacs-version)
