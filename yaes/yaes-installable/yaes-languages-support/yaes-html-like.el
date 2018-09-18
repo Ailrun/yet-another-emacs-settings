46f6b5f84d5096f936343a342cb32c667b3acd64
Junyoung Clare Jang <jjc9310@gmail.com>
2018-09-17 20:31:12 -0400

    Remove deleted package ac-html-bootstrap

--- yaes/yaes-installable/yaes-languages-support/yaes-html-like.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-html-like.el
@@ -56,22 +56,5 @@
   (add-hook 'jade-mode-hook #'company-web-jade-init)
   (add-hook 'slim-mode-hook #'company-web-slim-init))
 
-(req-package ac-html-bootstrap
-  :commands (company-web-bootstrap+
-             company-web-fa+)
-  :init
-  (add-hook 'html-mode-hook
-            (lambda ()
-              (company-web-bootstrap+)
-              (company-web-fa+)))
-  (add-hook 'jade-mode-hook
-            (lambda ()
-              (company-web-bootstrap+)
-              (company-web-fa+)))
-  (add-hook 'slim-mode-hook
-            (lambda ()
-              (company-web-bootstrap+)
-              (company-web-fa+))))
-
 (provide 'yaes-html-like)
 ;;; yaes-html-like.el ends here
