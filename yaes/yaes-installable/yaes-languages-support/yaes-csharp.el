b4076bc3577448ad23c232a746c9fb0ea01daacc
Junyoung Clare Jang <jjc9310@gmail.com>
2018-10-31 11:50:43 -0400

    Update csharp setting

--- yaes/yaes-installable/yaes-languages-support/yaes-csharp.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-csharp.el
@@ -13,12 +13,17 @@
 (req-package omnisharp
   :if (and
        (version<= "24.3" emacs-version)
-       (executable-find "curl")
-       (executable-find "OmniSharp"))
+       (executable-find "curl"))
   :require (json flycheck dash csharp-mode cl-lib s)
   :commands (omnisharp-mode)
   :init
-  (add-hook 'csharp-mode-hook #'omnisharp-mode))
+  (add-hook 'csharp-mode-hook #'omnisharp-mode)
+  (add-hook 'csharp-mode-hook
+            (lambda ()
+              (setq-local company-backends
+                          (append
+                           '(company-omnisharp)
+                           company-backends)))))
 
 (provide 'yaes-csharp)
 ;;; yaes-csharp.el ends here
