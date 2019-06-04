c208cd0eb754efef3407d4afb29470eec26ab02e
Ailrun <jjc9310@gmail.com>
2019-06-04 13:08:26 -0400

    Update transient levels

--- transient/levels.el
+++ transient/levels.el
@@ -1,4 +1,11 @@
 ((magit-am
   (magit:--gpg-sign . 4))
+ (magit-commit
+  (transient:magit-commit:--date= . 4)
+  (magit:--gpg-sign . 4))
+ (magit-merge
+  (magit:--gpg-sign . 4))
+ (magit-rebase
+  (magit:--gpg-sign . 4))
  (magit-revert
   (magit:--gpg-sign . 4)))
