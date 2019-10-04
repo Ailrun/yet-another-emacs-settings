0856fa8bf3496412111af1c778a0b8976f3fa2d8
Junyoung Clare Jang <jjc9310@gmail.com>
2019-10-04 15:47:57 -0400

    Update ocaml mode to use flycheck-ocaml

--- yaes/yaes-installable/yaes-languages-support/yaes-ocaml.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-ocaml.el
@@ -15,18 +15,20 @@
 (req-package merlin
   :if (executable-find "ocamlmerlin")
   :require (iedit tuareg)
-  :functions (yaes-merlin-setup)
   :commands (merlin-mode)
   :diminish (merlin-mode)
   :init
-  (defun yaes-merlin-setup ()
-    "Merlin setup."
-    (setq-local company-backends
-                (append
-                 '(merlin-company-backend)
-                 company-backends)))
   (add-hook 'tuareg-mode-hook #'merlin-mode)
-  (add-hook 'caml-mode-hook #'merlin-mode))
+  (add-hook 'caml-mode-hook #'merlin-mode)
+  :config
+  (setq merlin-error-after-save nil))
+
+(req-package flycheck-ocaml
+  :require (flycheck merlin)
+  :functions (flycheck-ocaml-setup)
+  :config
+  (add-hook 'tuareg-mode-hook #'flycheck-ocaml-setup)
+  (add-hook 'caml-mode-hook #'flycheck-ocaml-setup))
 
 (provide 'yaes-ocaml)
 ;;; yaes-ocaml.el ends here
