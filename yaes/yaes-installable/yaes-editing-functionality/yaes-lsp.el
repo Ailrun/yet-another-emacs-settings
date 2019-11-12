1a8594884cef817c6d13eafafaf69734548317a2
Junyoung Clare Jang <jjc9310@gmail.com>
2019-11-12 02:59:52 -0500

    Add LSP support

---
+++ yaes/yaes-installable/yaes-editing-functionality/yaes-lsp.el
@@ -0,0 +1,28 @@
+;;; yaes-lsp --- Language Server Protocol related package setting of yaes
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'req-package)
+
+(req-package lsp-mode
+  :if (version<= "25.1" emacs-version)
+  :require (dash dash-functional f ht spinner markdown-mode)
+  :commands (lsp)
+  :config
+  (setq lsp-prefer-flymake nil))
+
+(req-package lsp-ui
+  :if (version<= "25.1" emacs-version)
+  :require (dash dash-functional lsp-mode markdown-mode)
+  :commands (lsp-ui-mode)
+  :init (add-hook 'lsp-mode-hook #'lsp-ui-mode))
+
+(req-package company-lsp
+  :if (version<= "25.1" emacs-version)
+  :require (lsp-mode company s dash)
+  :config (push 'company-lsp company-backends))
+
+(provide 'yaes-lsp)
+;;; yaes-lsp.el ends here
