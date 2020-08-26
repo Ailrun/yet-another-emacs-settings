bd62d02a92d03791a8f3cd70f1b6bc99eca25db1
Junyoung Clare Jang <jjc9310@gmail.com>
2020-08-26 09:22:04 -0400

    Add xah-find

---
+++ yaes/yaes-installable/yaes-editing-functionality/yaes-xah-find.el
@@ -0,0 +1,12 @@
+;;; yaes-xah-find --- xah-find related package setting of yaes
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'req-package)
+
+(req-package xah-find)
+
+(provide 'yaes-xah-find)
+;;; yaes-xah-find.el ends here
