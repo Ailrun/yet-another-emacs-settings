096ec0aa1375fce619f423a7bb1bb7659778bc99
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-05-18 19:48:07 -0400

    Disable jdee for now

--- yaes/yaes-installable/yaes-languages-support/yaes-java.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-java.el
@@ -6,23 +6,27 @@
 
 (require 'req-package)
 
+(req-package lsp-java
+  :init
+  (add-hook 'java-mode-hook #'lsp))
+
 ;; Todo:
 ;; Let's try Meghanada
-(setq-default jdee-server-dir yaes-jdee-server-dir)
+;; (setq-default jdee-server-dir yaes-jdee-server-dir)
 
-(req-package jdee
-  :if (and
-       (version<= "24.3" emacs-version)
-       (file-directory-p yaes-jdee-server-dir))
-  :require (flycheck memoize dash)
-  :commands (jdee-mode))
+;; (req-package jdee
+;;   :if (and
+;;        (version<= "24.3" emacs-version)
+;;        (file-directory-p yaes-jdee-server-dir))
+;;   :require (flycheck memoize dash)
+;;   :commands (jdee-mode))
 
-(req-package mvn
-  :if (executable-find "mvn")
-  :commands (mvn mvn-compile mvn-clean mvn-test))
+;; (req-package mvn
+;;   :if (executable-find "mvn")
+;;   :commands (mvn mvn-compile mvn-clean mvn-test))
 
-(req-package java-snippets
-  :require (yasnippet))
+;; (req-package java-snippets
+;;   :require (yasnippet))
 
 (provide 'yaes-java)
 ;;; yaes-java.el ends here
