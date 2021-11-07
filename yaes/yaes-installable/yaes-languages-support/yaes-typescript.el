f622a0ffeb0db2bbad650f23ce68cfebd54dee84
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-11-06 20:32:40 -0400

    Use typescript lsp instead of tide

--- yaes/yaes-installable/yaes-languages-support/yaes-typescript.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-typescript.el
@@ -9,28 +9,30 @@
 (req-package typescript-mode
   :mode
   ("\\.tsx?\\'" . typescript-mode)
+  :init
+  (add-hook 'typescript-mode-hook #'lsp)
   :config
   (setq typescript-indent-level 2))
 
-(req-package tide
-  :if (executable-find "node")
-  :require (dash flycheck typescript-mode cl-lib)
-  :commands (tide-setup)
-  :diminish tide-mode
-  :init
-  (defun setup-tide-mode ()
-    "tide mode setup."
-    (interactive)
-    (tide-setup)
-    (if (eq system-type "windows-nt")
-        (setq flycheck-typescript-tslint-executable "tslint.cmd"))
-    (flycheck-mode t)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (flycheck-add-next-checker 'typescript-tide 'javascript-eslint)
-    (eldoc-mode t)
-    (tide-hl-identifier-mode t)
-    (company-mode t))
-  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+;; (req-package tide
+;;   :if (executable-find "node")
+;;   :require (dash flycheck typescript-mode cl-lib)
+;;   :commands (tide-setup)
+;;   :diminish tide-mode
+;;   :init
+;;   (defun setup-tide-mode ()
+;;     "tide mode setup."
+;;     (interactive)
+;;     (tide-setup)
+;;     (if (eq system-type "windows-nt")
+;;         (setq flycheck-typescript-tslint-executable "tslint.cmd"))
+;;     (flycheck-mode t)
+;;     (setq flycheck-check-syntax-automatically '(save mode-enabled))
+;;     (flycheck-add-next-checker 'typescript-tide 'javascript-eslint)
+;;     (eldoc-mode t)
+;;     (tide-hl-identifier-mode t)
+;;     (company-mode t))
+;;   (add-hook 'typescript-mode-hook #'setup-tide-mode))
 
 (provide 'yaes-typescript)
 ;;; yaes-typescript.el ends here
