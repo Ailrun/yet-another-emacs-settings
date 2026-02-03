067be05ed86462ebc163a3703a2d5b86750eae9b
Junyoung/"Clare" Jang <jjc9310@gmail.com>
2026-02-03 15:12:07 -0500

    Use lean4 mode instead

--- yaes/yaes-installable/yaes-languages-support/yaes-lean.el
+++ yaes/yaes-installable/yaes-languages-support/yaes-lean.el
@@ -6,21 +6,16 @@
 
 (require 'use-package)
 
-(use-package lean-mode
+(use-package lean4-mode
   :if (version<= "24.3" emacs-version)
-  :after (flycheck)
+  :vc (:url "https://github.com/leanprover-community/lean4-mode.git"
+            :rev :last-release)
   :mode
-  ("\\.lean\\'" . lean-mode)
+  ("\\.lean\\'" . lean4-mode)
   :interpreter
-  ("lean" . lean-mode)
+  ("lean" . lean4-mode)
   :custom
   (lean-message-boxes-enabledp t))
-
-(use-package company-lean
-  :if (version<= "24.3" emacs-version)
-  :after (company lean-mode)
-  :hook
-  (lean-mode . company-lean-hook))
 
 (provide 'yaes-lean)
 ;;; yaes-lean.el ends here
